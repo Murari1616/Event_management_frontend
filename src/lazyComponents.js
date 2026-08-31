fccafca505a2742f8978b3a3d107fce0fbb1d1eb
Murari_K <166288154+Murari1616@users.noreply.github.com>
2026-08-31 20:49:04 +0530

    Add files via upload

--- src/lazyComponents.js
+++ src/lazyComponents.js
@@ -2,3 +2,4 @@ import { lazy } from "react";
 
 export const Register = lazy(() => import("./pages/registration"));
 export const RegisteredUsers = lazy(() => import("./pages/RegisteredUsers"));
+export const EventsManagement = lazy(() => import("./pages/event/eventManagement"));
